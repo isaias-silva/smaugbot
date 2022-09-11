722ffe07753d284f7d6b019bb2b706a2301f452f
isaias-silva <zackblackmangold@gmail.com>
2022-09-10 22:10:27 -0300

    refact

--- src/bot/bot.ts
+++
@@ -1,9 +0,0 @@
-import { connect } from "./connect";
-
-export async function bot(io: any) {
-
-    const socket = await connect(io, io.id)
-    socket.ev.on('messages.upsert', (message) => {
-       const [wmessage]=message.messages
-    })
-}
