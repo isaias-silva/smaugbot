ddb80f90b1af8b7e3243fcfda218b02e20c1c73c
isaias-silva <zackblackmangold@gmail.com>
2022-09-11 21:18:23 -0300

    poo

---
+++ src/bot.ts
@@ -0,0 +1,8 @@
+import { Socket } from "./socket"
+
+export class Bot{
+    start= async()=>{
+        const socket=await new Socket().connect()
+    //
+    }
+}
---
+++ src/socket.ts
@@ -0,0 +1,41 @@
+import makeWaSocket, {
+    DisconnectReason,
+    useSingleFileAuthState,
+} from "@adiwajshing/baileys";
+import { Boom } from "@hapi/boom";
+import path from "path";
+
+
+export class Socket {
+    connect = async () => {
+
+        const { state, saveState } = useSingleFileAuthState(
+            path.resolve(__dirname, "..", "cache", "auth.json")
+        );
+
+        const socket = makeWaSocket({
+            printQRInTerminal: true,
+            auth: state,
+            defaultQueryTimeoutMs: undefined,
+
+
+        });
+
+        socket.ev.on("connection.update", async (update) => {
+            const { connection, lastDisconnect } = update;
+            if (connection === "close") {
+                const shouldReconnect =
+                    (lastDisconnect?.error as Boom)?.output?.statusCode !==
+                    DisconnectReason.loggedOut;
+
+                if (shouldReconnect) {
+                    await this.connect();
+                }
+            }
+        });
+
+        socket.ev.on("creds.update", saveState);
+
+        return socket;
+    };
+}
