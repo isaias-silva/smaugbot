9a6efd67cdfb0473711df3765fc2c4ea6b893598
isaias-silva <zackblackmangold@gmail.com>
2022-09-28 16:52:37 -0300

    add tudo

--- public/script/script.js
+++ public/script/script.js
@@ -30,20 +30,29 @@ console.log(msg)
 
 sock.on('msgTxt',(msg)=>{
     const conversa=document.querySelector('#conversas')
-  
-conversa.innerHTML+=`   <div class="msgs">
+  let id='id_'+Math.random().toString(36).replace('.','')
+conversa.innerHTML+=`   <div id='${id}' class="msgs">
 <img class="user" src="${msg.perfil}" alt="image">
 <p>
    <b>${msg.numero}</b>: ${msg.message}
 </p>
 <button value='${JSON.stringify(msg)}' onclick='response(this.value)' class='send'>&#9993;</button>
-<button  class='close'>&#10008;</button>
+<button value='${id}' onclick='deletee(this.value)' class='close'>&#10008;</button>
 </div>
 </div>`
 
 })
 
 //buttons events
+function deletee(id){
+    const element= document.getElementById(id)
+    element.style.filter='opacity(0)'
+    setTimeout(()=>{if (element.parentNode) {
+       
+        element.parentNode.removeChild(element);
+          }},2000)
+    
+}
 function response(msg){
     const mesage=JSON.parse(msg)
     let message=prompt(`digite a resposta para ${mesage.numero}`)
